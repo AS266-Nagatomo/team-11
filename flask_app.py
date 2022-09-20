84abc0225bef996ed10bb786a8931494eacff1f7
{hideki} <{gotooooo0810@gmail.com}>
2022-09-20 13:36:21 +0900

    13:36 commit

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
-app = Flask(__name__, static_url_path='/')
+app = Flask(__name__, static_url_path='/',)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
