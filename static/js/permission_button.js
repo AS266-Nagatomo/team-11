ff1535503999c50e1c35a4b9f9a982e00fbb4d69
AS266-Nagatomo <souya.codegym@gmail.com>
2022-09-26 14:25:14 +0000

    Mypage search

---
+++ static/js/permission_button.js
@@ -0,0 +1,9 @@
+//$('input.cb-value').prop("checked", true);
+$('.cb-value').click(function() {
+    var mainParent = $(this).parent('.toggle-btn');
+    if($(mainParent).find('input.cb-value').is(':checked')) {
+      $(mainParent).addClass('active');
+    } else {
+      $(mainParent).removeClass('active');
+    }
+  })
